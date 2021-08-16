18c9121fc6f4a71ab14ee340bdddf6158cb66664
Selman Jir CAN <78292930+selmanjir@users.noreply.github.com>
2021-08-16 17:58:01 +0300

    Update ToLendfrm.Designer.cs

--- LibraryProject.NetFramework/ToLendfrm.Designer.cs
+++ LibraryProject.NetFramework/ToLendfrm.Designer.cs
@@ -1,4 +1,4 @@
-﻿
+
 namespace LibraryProject.NetFramework
 {
     partial class ToLendfrm
@@ -29,6 +29,8 @@ namespace LibraryProject.NetFramework
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ToLendfrm));
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.ToLendInformation = new System.Windows.Forms.GroupBox();
             this.label12 = new System.Windows.Forms.Label();
@@ -36,21 +38,29 @@ namespace LibraryProject.NetFramework
             this.label11 = new System.Windows.Forms.Label();
             this.txtUserName = new System.Windows.Forms.TextBox();
             this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
-            this.txtBookName = new System.Windows.Forms.TextBox();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.label5 = new System.Windows.Forms.Label();
             this.txtEmployee = new System.Windows.Forms.TextBox();
             this.label7 = new System.Windows.Forms.Label();
             this.btnAdd = new System.Windows.Forms.Button();
             this.btnCancel = new System.Windows.Forms.Button();
+            this.comboBookId = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.ToLendInformation.SuspendLayout();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
-            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonShadow;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.ControlDarkDark;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.ButtonFace;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle1;
             this.dataGridView1.Location = new System.Drawing.Point(236, 13);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(632, 315);
@@ -60,16 +70,18 @@ namespace LibraryProject.NetFramework
             // ToLendInformation
             // 
             this.ToLendInformation.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ToLendInformation.BackgroundImage = global::LibraryProject.NetFramework.Properties.Resources.eTgd2o2;
+            this.ToLendInformation.Controls.Add(this.comboBookId);
             this.ToLendInformation.Controls.Add(this.label12);
             this.ToLendInformation.Controls.Add(this.label13);
             this.ToLendInformation.Controls.Add(this.label11);
             this.ToLendInformation.Controls.Add(this.txtUserName);
             this.ToLendInformation.Controls.Add(this.dateTimePicker2);
-            this.ToLendInformation.Controls.Add(this.txtBookName);
             this.ToLendInformation.Controls.Add(this.dateTimePicker1);
             this.ToLendInformation.Controls.Add(this.label5);
             this.ToLendInformation.Controls.Add(this.txtEmployee);
             this.ToLendInformation.Controls.Add(this.label7);
+            this.ToLendInformation.ForeColor = System.Drawing.SystemColors.ButtonFace;
             this.ToLendInformation.Location = new System.Drawing.Point(12, 13);
             this.ToLendInformation.Name = "ToLendInformation";
             this.ToLendInformation.Size = new System.Drawing.Size(200, 190);
@@ -81,6 +93,8 @@ namespace LibraryProject.NetFramework
             // label12
             // 
             this.label12.AutoSize = true;
+            this.label12.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label12.ForeColor = System.Drawing.SystemColors.ButtonFace;
             this.label12.Location = new System.Drawing.Point(26, 139);
             this.label12.Name = "label12";
             this.label12.Size = new System.Drawing.Size(57, 13);
@@ -91,6 +105,8 @@ namespace LibraryProject.NetFramework
             // label13
             // 
             this.label13.AutoSize = true;
+            this.label13.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label13.ForeColor = System.Drawing.SystemColors.ButtonFace;
             this.label13.Location = new System.Drawing.Point(45, 38);
             this.label13.Name = "label13";
             this.label13.Size = new System.Drawing.Size(38, 13);
@@ -100,6 +116,8 @@ namespace LibraryProject.NetFramework
             // label11
             // 
             this.label11.AutoSize = true;
+            this.label11.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label11.ForeColor = System.Drawing.SystemColors.ButtonFace;
             this.label11.Location = new System.Drawing.Point(17, 113);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(66, 13);
@@ -123,13 +141,6 @@ namespace LibraryProject.NetFramework
             this.dateTimePicker2.Value = new System.DateTime(2021, 8, 9, 0, 0, 0, 0);
             this.dateTimePicker2.ValueChanged += new System.EventHandler(this.dateTimePicker2_ValueChanged);
             // 
-            // txtBookName
-            // 
-            this.txtBookName.Location = new System.Drawing.Point(89, 61);
-            this.txtBookName.Name = "txtBookName";
-            this.txtBookName.Size = new System.Drawing.Size(100, 20);
-            this.txtBookName.TabIndex = 5;
-            // 
             // dateTimePicker1
             // 
             this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
@@ -142,6 +153,8 @@ namespace LibraryProject.NetFramework
             // label5
             // 
             this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonFace;
             this.label5.Location = new System.Drawing.Point(40, 64);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(43, 13);
@@ -158,6 +171,8 @@ namespace LibraryProject.NetFramework
             // label7
             // 
             this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label7.ForeColor = System.Drawing.SystemColors.ButtonFace;
             this.label7.Location = new System.Drawing.Point(23, 90);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(60, 13);
@@ -166,34 +181,48 @@ namespace LibraryProject.NetFramework
             // 
             // btnAdd
             // 
+            this.btnAdd.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnAdd.ForeColor = System.Drawing.SystemColors.ButtonFace;
             this.btnAdd.Location = new System.Drawing.Point(12, 209);
             this.btnAdd.Name = "btnAdd";
             this.btnAdd.Size = new System.Drawing.Size(75, 23);
             this.btnAdd.TabIndex = 3;
             this.btnAdd.Text = "Ödünç Ver";
-            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.UseVisualStyleBackColor = false;
             this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
             // btnCancel
             // 
+            this.btnCancel.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnCancel.ForeColor = System.Drawing.SystemColors.ButtonFace;
             this.btnCancel.Location = new System.Drawing.Point(137, 209);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(75, 23);
             this.btnCancel.TabIndex = 5;
             this.btnCancel.Text = "İptal";
-            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.UseVisualStyleBackColor = false;
             this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // comboBookId
+            // 
+            this.comboBookId.FormattingEnabled = true;
+            this.comboBookId.Location = new System.Drawing.Point(89, 60);
+            this.comboBookId.Name = "comboBookId";
+            this.comboBookId.Size = new System.Drawing.Size(100, 21);
+            this.comboBookId.TabIndex = 6;
             // 
             // ToLendfrm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.ClientSize = new System.Drawing.Size(902, 388);
+            this.BackgroundImage = global::LibraryProject.NetFramework.Properties.Resources.eTgd2o2;
+            this.ClientSize = new System.Drawing.Size(898, 354);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.ToLendInformation);
             this.Controls.Add(this.dataGridView1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "ToLendfrm";
             this.Text = "Ödünç Kitap Ver Sayfası";
             this.Load += new System.EventHandler(this.ToLendfrm_Load);
@@ -218,7 +247,7 @@ namespace LibraryProject.NetFramework
         private System.Windows.Forms.Button btnCancel;
         private System.Windows.Forms.Label label13;
         private System.Windows.Forms.TextBox txtUserName;
-        private System.Windows.Forms.TextBox txtBookName;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBookId;
     }
 }
